36b39cd6f46b40d23127e030fb3a8ac5d505778b
chandidasgit <chandidas.4@gmail.com>
2017-12-23 11:57:59 +0530

    two file

---
+++ simpleintrest.java
@@ -0,0 +1,16 @@
+class simpleintrest
+{
+public static void main(string [] args)
+{
+int p,r,t,omi,toti,total;
+p=5000;
+r=2;
+t=15;
+omi=5000*2/100;
+system.out.pri9ntln("The one month Intrestis "+omi);
+toti=omi*t;
+system.out.println("The total Intrest is "+toti);
+total=p+toti;
+system.out.println("The Total payble Amount is "+total);
+}
+}
